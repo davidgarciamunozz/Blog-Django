11505021ea6ef9c65af9c413425aca502a7f31e4
davidgarciamunozz <garxia0710@gmail.com>
2024-06-22 11:52:03 -0500

    lastMod

---
+++ src/Routes.js
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+import Error404 from 'containers/errors/Error404';
+import Home from 'containers/pages/Home';
+import Cases from 'containers/pages/Cases';
+
+import Services from 'containers/pages/Services';
+import About from 'containers/pages/About';
+import Careers from 'containers/pages/Careers';
+import Blog from 'containers/pages/Blog';
+import Contact from 'containers/pages/Contact';
+
+import { AnimatePresence } from 'framer-motion';
+
+function AppRoutes(){
+
+    const location = useLocation()
+
+    return(
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                    {/* Error Display */}
+                    <Route path="*" element={<Error404 />} />
+
+                    {/* Home Display */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/cases" element={<Cases />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/careers" element={<Careers />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+        </AnimatePresence>
+    )
+}
+export default AppRoutes
