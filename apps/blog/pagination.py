ce56b5d3f06d311e1a88057c5ddb35c27e15048b
davidgarciamunozz <garxia0710@gmail.com>
2024-06-22 22:58:20 -0500

    lastMod

--- apps/blog/pagination.py
+++ apps/blog/pagination.py
@@ -2,9 +2,9 @@ from rest_framework.pagination import PageNumberPagination
 
 class SmallSetPagination(PageNumberPagination):
     page_query_param = 'p'
-    page_size = 6
+    page_size = 5
     page_size_query_param = 'page_size'
-    max_page_size = 6
+    max_page_size = 5
 
 class MediumSetPagination(PageNumberPagination):
     page_query_param = 'p'
